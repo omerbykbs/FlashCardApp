40571975a1ddc957ce5a81630491a8845e3f7843
Omer Faruk Buyukbas <68295684+omerbykbs@users.noreply.github.com>
2024-12-11 18:25:33 +0100

    Update

--- activerecall2/src/main/java/de/unibremen/swp/activerecall2/gui/LearningExercisesWindow.java
+++ activerecall2/src/main/java/de/unibremen/swp/activerecall2/gui/LearningExercisesWindow.java
@@ -17,7 +17,7 @@ import java.util.List;
 /**
  * This class creates the Learning Exercises window of the Application.
  *
- * @author Ömer Faruk Büyükbas
+ * @author Ömer Faruk Büyükbas, Oguzhan Aydin, Kamil Uyanik
  */
 public class LearningExercisesWindow extends JFrame {
     /**
