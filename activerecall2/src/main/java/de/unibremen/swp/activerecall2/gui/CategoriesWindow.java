ef60069a8b93cd2a9d2449fc78713e337ed2a0b2
Omer Faruk Buyukbas <68295684+omerbykbs@users.noreply.github.com>
2024-12-11 18:24:18 +0100

    Update

--- activerecall2/src/main/java/de/unibremen/swp/activerecall2/gui/CategoriesWindow.java
+++ activerecall2/src/main/java/de/unibremen/swp/activerecall2/gui/CategoriesWindow.java
@@ -18,7 +18,7 @@ import java.util.List;
 /**
  * This class creates the Categories window of the Application.
  *
- * @author Ömer Faruk Büyükbas
+ * @author Ömer Faruk Büyükbas, Oguzhan Aydin, Kamil Uyanik
  */
 public class CategoriesWindow extends JFrame {
     /**
